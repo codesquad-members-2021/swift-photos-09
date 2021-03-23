24b9ef3288ae148cdaf8c5a12dd9ba264365dcc3
HongzCloud <jihong21511@gmail.com>
2021-03-23 14:31:48 +0900

    ✨ Add PHAsset

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -6,26 +6,31 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
  
     @IBOutlet weak var collectionView: UICollectionView!
+    var allPhotos: PHFetchResult<PHAsset>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.register(ColorCell.nib(), forCellWithReuseIdentifier: ColorCell.identifier)
         collectionView.dataSource = self
         collectionView.delegate = self
+        
+        allPhotos = PHAsset.fetchAssets(with: nil)
     }
 }
 
 extension ViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 40
+        return allPhotos.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let asset = allPhotos.object(at: indexPath.item)
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ColorCell.identifier, for: indexPath) as! ColorCell
         return cell
