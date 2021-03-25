7d4febf746e156916ac20d66f6661f5eec026d69
1song2 <1song2918@gmail.com>
2021-03-25 18:09:43 +0900

    ✨ Add save photo function

--- PhotosApp/PhotosApp/DoodleViewCell.swift
+++ PhotosApp/PhotosApp/DoodleViewCell.swift
@@ -10,17 +10,7 @@ import UIKit
 class DoodleViewCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     
-    //override init(frame: CGRect) {
-    //    super.init(frame: frame)
-    //    commonInit()
-    //}
-    //
-    //required init?(coder: NSCoder) {
-    //    super.init(coder: coder)
-    //    commonInit()
-    //}
-    //
-    //func commonInit() {
-    //    imageView.contentMode = .scaleAspectFill
-    //}
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
 }
--- PhotosApp/PhotosApp/DoodleViewController.swift
+++ PhotosApp/PhotosApp/DoodleViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class DoodleViewController: UICollectionViewController {
     
+    var savedItem: DoodleViewCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView.backgroundColor = .darkGray
@@ -16,11 +18,46 @@ class DoodleViewController: UICollectionViewController {
         let closeButton = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(closeButtonPressed))
         self.navigationItem.rightBarButtonItem = closeButton
         self.navigationItem.title = "Doodles"
+        
+        let longPressGesutreRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(cellDidPress))
+        longPressGesutreRecognizer.minimumPressDuration = 0.3
+        collectionView.addGestureRecognizer(longPressGesutreRecognizer)
     }
     
     @objc
     func closeButtonPressed() {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc
+    func cellDidPress(sender: UILongPressGestureRecognizer) {
+        let point = sender.location(in: collectionView)
+        let indexPath = self.collectionView.indexPathForItem(at: point)
+        if let indexPath = indexPath {
+            let cell = self.collectionView.cellForItem(at: indexPath) as! DoodleViewCell
+            savedItem = cell
+            cell.becomeFirstResponder()
+            let saveMenuItem = UIMenuItem(title: "Save", action: #selector(savePhoto))
+            UIMenuController.shared.menuItems = [saveMenuItem]
+            UIMenuController.shared.showMenu(from: collectionView, rect: cell.frame)
+        }
+    }
+    
+    @objc
+    func savePhoto(sender: UIMenuController) {
+        UIImageWriteToSavedPhotosAlbum(savedItem.imageView.image ?? UIImage(), self, #selector(image), nil)
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
     }
     
     func parseJson(fileName: String) -> [DoodleModel]? {
@@ -62,6 +99,10 @@ extension DoodleViewController {
                 task.resume()
             }
         }
+        
+        let longPressGesutreRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(cellDidPress))
+        longPressGesutreRecognizer.minimumPressDuration = 0.3
+        cell.addGestureRecognizer(longPressGesutreRecognizer)
         
         return cell
     }
